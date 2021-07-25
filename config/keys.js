6fc739f6057f9248e2f6ec0267b968e424b45fa5
Basit Mir <80674953+basitmir7@users.noreply.github.com>
2021-07-25 22:44:22 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
+// place your mongodb cluster name and password 
 module.exports = {
   mongoURI:
     "mongodb+srv://basitmir7:basit@0222@cluster0.e6chw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
